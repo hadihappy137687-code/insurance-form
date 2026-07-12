228710f91792b468060674823731712efce7ae77
hadihappy137687-code <hadi.happy.1376.8.7@gmail.com>
2026-07-12 20:04:56 +0330

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,64 @@ form.addEventListener("submit", async function (e) {
             body: JSON.stringify(data)
         });
 
-        alert("✅ درخواست شما با موفقیت ثبت شد.");
+        const trackingCode =
+"BH-" + Date.now().toString().slice(-6);
 
+const discountCode =
+"HYD-" + Math.random().toString(36).substring(2,7).toUpperCase();
+
+document.querySelector(".container").innerHTML = `
+
+<div style="text-align:center;padding:40px">
+
+<h2 style="color:#0b5ed7">
+✅ درخواست شما با موفقیت ثبت شد
+</h2>
+
+<p style="margin-top:20px">
+
+کارشناسان نمایندگی بیمه حیدریان
+در اولین فرصت با شما تماس خواهند گرفت.
+
+</p>
+
+<div style="margin-top:30px;background:#eef5ff;padding:20px;border-radius:15px">
+
+<h3>🆔 کد پیگیری</h3>
+
+<h2>${trackingCode}</h2>
+
+</div>
+
+<div style="margin-top:20px;background:#fff7e6;padding:20px;border-radius:15px">
+
+<h3>🎁 کد هدیه</h3>
+
+<h2>${discountCode}</h2>
+
+<p>
+
+این کد تخفیف را هنگام تماس یا مراجعه به نمایندگی اعلام کنید.
+
+</p>
+
+</div>
+
+<br>
+
+<a href="tel:09146876636">
+
+<button>
+
+تماس با نمایندگی
+
+</button>
+
+</a>
+
+</div>
+
+`;
         form.reset();
 
     } catch (err) {
